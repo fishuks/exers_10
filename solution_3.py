8642153166954ba76c54b36a36c8402ed83c790f
fishukaa <103937450+fishuks@users.noreply.github.com>
2024-04-14 23:46:41 +0700

    fix errors in task3

--- solution_3.py
+++ solution_3.py
@@ -1,6 +1,7 @@
 import random
 class NavalBattle:
     playing_field = []
+  
 
     def __init__(self, symbol):
         self.symbol = symbol
@@ -31,11 +32,57 @@ class NavalBattle:
 
     @staticmethod
     def new_game():
-        pass
+        NavalBattle.playing_field = [[0 for _ in range(10)] for _ in range(10)]  
+        ships = [(4, 1), (3, 2), (2, 3), (1, 4)]  
+
+        for ship_size, ship_count in ships:
+            for _ in range(ship_count):
+                while True:
+                    x = random.randint(0, 9) 
+                    y = random.randint(0, 9) 
+                    orient = random.choice(['h', 'v'])  
+
+                    if orient == 'h' and x + ship_size <= 10: 
+                        valid = all(0 <= x+i < 10 and NavalBattle.playing_field[y][x+i] == 0 for i in range(ship_size)) 
+                        if valid:
+                            intersect = False
+                            for i in range(ship_size):
+                                if any(0 <= y+j < 10 and 0 <= x+i < 10 and NavalBattle.playing_field[y+j][x+i] == 1 for j in [-1, 0, 1] for i in [-1, 0, 1]):
+                                    intersect = True
+                                    break
+                            if not intersect:
+                                for i in range(ship_size):
+                                    NavalBattle.playing_field[y][x+i] = 1  
+                                break
+
+                    elif orient == 'v' and y + ship_size <= 10: 
+                        valid = all(0 <= y+i < 10 and NavalBattle.playing_field[y+i][x] == 0 for i in range(ship_size))  
+                        if valid:
+                            intersect = False
+                            for i in range(ship_size):
+                                if any(0 <= y+i < 10 and 0 <= x+j < 10 and NavalBattle.playing_field[y+i][x+j] == 1 for j in [-1, 0, 1] for i in [-1, 0, 1]):
+                                    intersect = True
+                                    break
+                            if not intersect:
+                                for i in range(ship_size):
+                                    NavalBattle.playing_field[y+i][x] = 1  
+                                break
+
+        print(NavalBattle.playing_field)
+        for row in NavalBattle.playing_field:
+            for cell in row:
+                if cell == 0:
+                    print(0, end = ' ')
+                elif cell == 1:
+                    print(1, end=" ")
+                else:
+                    print(cell, end=" ")
+            print()
+
+        return NavalBattle.playing_field
 
     def __str__(self):
         return f'{self.symbol}'
 
     def __repr__(self):
         return f'{self.symbol}'
-
--- solution_5.py
+++ solution_5.py
@@ -1,18 +1,30 @@
 class RomanNumber:
 
-    def __init__(self, rom_value):
-        if self.is_roman(rom_value):
-            self.rom_value = rom_value
+    def __init__(self, value):
+        if self.is_roman(value):
+            self.rom_value = value
+            self.int_value = self.decimal_number()
+        elif self.is_int(value):
+            self.int_value = value
+            self.rom_value = self.roman_number()
         else:
-            print('ошибка')
+            print('Ошибка')
             self.rom_value = None
+            self.int_value = None
 
+    @staticmethod
+    def is_int(value):
+        if isinstance(value, int) and value > 0:
+            return True
+        else:
+            return False
+        
     @staticmethod
     def is_roman(value):
         roman_nums = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
         prev_sign = 0
         count = 1
-        for sign in value:
+        for sign in str(value):
             if sign not in roman_nums:
                 return False
             if prev_sign == sign:
@@ -45,6 +57,16 @@ class RomanNumber:
             prev_value = value
 
         return decimal_num
+    
+    def roman_number(self):
+        roman_dict = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C',
+                      400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
+        result = ''
+        for value, numeral in sorted(roman_dict.items(), reverse=True):
+            while self.int_value >= value:
+                result += numeral
+                self.int_value -= value
+        return result
 
     def __str__(self):
         return f'{self.rom_value}'
