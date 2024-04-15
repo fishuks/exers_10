ba1861c6fd33aa20493304413a1bba1644a2f81b
fishukaa <103937450+fishuks@users.noreply.github.com>
2024-04-15 18:00:59 +0700

    del cash

--- test5.py
+++ test5.py
@@ -13,23 +13,3 @@ print(num_3.rom_value)
 print(num_3)
 print(RomanNumber.is_int(-614))
 print(RomanNumber.is_int(3758))
-{'I': 1,
-                      'IV' : 4,
-                      'IV' : 6,
-                      'V': 5,
-                      'IX' : 9,
-                      'X': 10,
-                      'XI' : 11,
-                      'XL' : 40,
-                      'L': 50,
-                      'LX' : 60,
-                      'XC' : 90,
-                      'C': 100,
-                      'CX' : 110,
-                      'CD' : 400,
-                      'D': 500,
-                      'DC' : 600,
-                      'CM' : 900,
-                      'M': 1000,
-                        'MC' : 1100
-                      }
